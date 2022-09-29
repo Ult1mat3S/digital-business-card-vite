4e78112b6d40baba6b0bc0df9df848bc8c1faf91
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2022-09-29 02:44:20 -0600

    feat: add scripts to deploy on github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 
 export default defineConfig({
+  base: "/digital-business-card-vite/",
   server: {
     open: true,
   },
